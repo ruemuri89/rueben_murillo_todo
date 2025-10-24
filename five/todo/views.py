e9856c816a8c2bb4e6d06aa45eb6a1a0067ea0df
Rueben Murillo <ruemurillo@gmail.com>
2025-10-24 10:23:33 -0500

    editing .urls file

--- five/todo/views.py
+++ five/todo/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import ToDo
+from .forms import ToDoForm
 
 # Create your views here.
+def task_list(request):
+    tasks = ToDo.objects.all().order_by("due_date")
+    return render(request, "todo/task_list.html", {"tasks": tasks})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = ToDoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = ToDoForm()
+    return render(request, "todo/task_form.html", {"form": form})
