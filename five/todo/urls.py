1cb1873c5fb713a8f05e2a005c2b59de9d656077
Rueben Murillo <ruemurillo@gmail.com>
2025-10-24 13:04:52 -0500

    added files and updated readme

--- five/todo/urls.py
+++ five/todo/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("task/", views.task_list, name="task_list"),
-    path("task/create/", views.create_task, name="task_create"),
-    path("task/<int:pk>/edit/", views.edit_task, name="task_edit"),
-    path("task/<int:pk>/delete/", views.delete_task, name="task_delete"),
-    path("task/<int:pk>/toggle/", views.toggle_task, name="task_toggle"),
+    path('', views.task_list, name='task_list'),
+    path('create/', views.create_task, name='create_task'),
+    path('edit/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete/<int:pk>/', views.delete_task, name='delete_task'),
+    path('toggle/<int:pk>/', views.toggle_task, name='toggle_task'),
 ]
--- five/todo/views.py
+++ five/todo/views.py
@@ -7,7 +7,7 @@ def task_list(request):
     tasks = ToDo.objects.all().order_by("due_date")
     return render(request, "todo/task_list.html", {"tasks": tasks})
 
-def task_create(request):
+def create_task(request):
     if request.method == 'POST':
         form = ToDoForm(request.POST)
         if form.is_valid():
@@ -16,3 +16,27 @@ def task_create(request):
     else:
         form = ToDoForm()
     return render(request, "todo/task_form.html", {"form": form})
+
+def edit_task(request, pk):
+    task = get_object_or_404(ToDo, pk=pk)
+    if request.method == 'POST':
+        form = ToDoForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_list")
+    else:
+        form = ToDoForm(instance=task)
+    return render(request, "todo/task_form.html", {"form": form})
+
+def delete_task(request, pk):
+    task = get_object_or_404(ToDo, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect("task_list")
+    return render(request, "todo/task_confirm_delete.html", {"task": task})
+
+def toggle_task(request, pk):
+    task = get_object_or_404(ToDo, pk=pk)
+    task.completed = not task.completed
+    task.save()
+    return redirect("task_list")
