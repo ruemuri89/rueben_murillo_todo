ef08d4cd724c66b1ec5a185243cc198226e64b01
ruemuri89 <ruemurillo@gmail.com>
2025-10-27 13:55:21 -0500

    added Admin model to admin.py

--- five/todo/admin.py
+++ five/todo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ToDo
 
 # Register your models here.
+@admin.register(ToDo)
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'completed', 'due_date')
+    list_filter = ('completed', 'due_date')
+    search_fields = ('name', 'description')
+    
+    fieldsets = (
+        ('Todo Details', {
+            'fields': ('name', 'description', 'due_date')
+        }),
+        ('Status', {
+            'fields': ('completed','overdue')
+        }),
